1eaef67d404c77f71755693fd4cee5e392d0bcc4
Oğulcan TURAN <ismetogulcanturan@gmail.com>
2020-02-26 23:59:16 +0300

    Add files via upload

---
+++ Utility/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiziSearch.Utility
+{
+    public class Constants
+    {
+        public const string ImageFolder = @"images\DiziImages";
+        public const string DefaultImage = @"default-image.png";
+
+        public const string MasterAdminUser = "0";
+        public const string NormalAdminUser = "1";
+        public const string ModeratorUser = "2";
+    }
+}
